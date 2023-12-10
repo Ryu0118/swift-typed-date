c14ccde23c58d0ef63efe715f6f6b1f1ca31b21a
Ryu <87907656+Ryu0118@users.noreply.github.com>
2023-12-10 22:00:09 +0900

    Update TypedDateProtocol.swift

--- Sources/TypedDate/TypedDateProtocol.swift
+++ Sources/TypedDate/TypedDateProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 /// The base protocol for typed date
-public protocol TypedDateProtocol: Hashable, Codable, Equatable {
+public protocol TypedDateProtocol: Hashable, Codable, Equatable, Comparable {
     var date: Date { get }
 }
