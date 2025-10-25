ba5bdaf4d352ad14fc9fc83a61bdd70ddb254984
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2025-10-25 19:49:45 +0900

    Remove swift-testing dependency

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ let package = Package(
     dependencies: [
         // Depend on the Swift 6.0 release of SwiftSyntax
         .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
-        .package(url: "https://github.com/apple/swift-testing.git", from: "0.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.1")
     ],
     targets: [
@@ -67,7 +66,6 @@ let package = Package(
             name: "TypedDateTests",
             dependencies: [
                 "TypedDate",
-                .product(name: "Testing", package: "swift-testing"),
             ]
         )
     ]
